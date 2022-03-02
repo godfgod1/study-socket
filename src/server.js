46a4d40790726facc4a056cc8ed7c87870e552c1
godfgod1 <godfgod1@naver.com>
2022-03-02 21:10:13 +0900

    1.6

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 import http from 'http'
 import express from "express";
 import WebSocket from 'ws';
+import { SocketAddress } from 'net';
 
 const app = express();
 
 //! 렌더링
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
+
 //! 유저가 서버를 보면 안되기 때문에, 유저가 볼 수 있는 폴더만 공유해주는 일이 express.static
 app.use("/public", express.static(__dirname+"/public"));
 app.get("/", (req,res) => res.render("home"));
@@ -15,12 +17,21 @@ app.get("/*", (req,res)=>res.redirect("/"))
 const handleListen = () => console.log("Listening on http://localhost:5000")
 
 const server = http.createServer(app)
-const wss = new WebSocket.server({ server})
+const wss = new WebSocket.Server({ server})
 
-function handleConnection(socket){
-  console.log(socket)
-}
+const sockets = []
 
-wss.on("connection", handleConnection)
+wss.on("connection", (socket)=>{
+  sockets.push(socket)
+  console.log('sockets',sockets)
+  console.log('Connected to Browser')
+  socket.send('hello!!')
+  socket.on("close", ()=> console.log("Disconnected from the Broswer"))
+  socket.on("message", message =>{
+    sockets.forEach(aSocket =>{
+      aSocket.send(message.toLocaleString('utf8'))
+    })
+  })
+})
 
 server.listen(5000, handleListen)
